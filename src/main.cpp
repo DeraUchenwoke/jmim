12eced056fe787404e059545a86a41a9c0508b07
Dera Uchenwoke <uchidera1@gmail.com>
2024-03-02 20:59:57 +0000

    move windows speciifc read file / get permissions to separate project to benchmark - KISS

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,2 @@
-#include "../include/ReadPerms.hpp"
-
 int main() {
-    std::cout << '\n';
-    std::wstring username = GetCurrentUsername(); 
-    std::wstring domain_name = GetDomainName(); 
-    std::wcout << CreateQualifiedAccountName(username, domain_name) << std::endl;
 }
